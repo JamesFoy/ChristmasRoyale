1784f2c1fb1498827863e462cef3ff1b690e26ad
JamesFoy <39011272+JamesFoy@users.noreply.github.com>
2019-12-06 15:30:12 +0000

    Started changing character to merry fragmas

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine.Events;
+using Mirror;
+using UnityEngine;
+
+[System.Serializable]
+public class ToggleEvent : UnityEvent<bool> { }
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField] ToggleEvent onToggleShared;
+    [SerializeField] ToggleEvent onToggleLocal;
+    [SerializeField] ToggleEvent onToggleRemote;
+
+    GameObject mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main.gameObject;
+
+        EnablePlayer();
+    }
+
+    void DisablePlayer()
+    {
+        onToggleShared.Invoke(false);
+
+        if (isLocalPlayer)
+        {
+            mainCamera.SetActive(true);
+            onToggleLocal.Invoke(false);
+        }
+        else
+        {
+            onToggleRemote.Invoke(false);
+        }
+    }
+
+    void EnablePlayer()
+    {
+        onToggleShared.Invoke(true);
+
+        if (isLocalPlayer)
+        {
+            mainCamera.SetActive(false);
+            onToggleLocal.Invoke(true);
+        }
+        else
+        {
+            onToggleRemote.Invoke(true);
+        }
+    }
+}
